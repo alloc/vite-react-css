863f177f441c3f8de6ff8b229276b9de55ea1af4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-04-16 00:15:28 -0400

    fix: let `scoped` param be anywhere in the querystring

--- src/regex.ts
+++ src/regex.ts
@@ -1,5 +1,6 @@
 export const extensionRE = /\.[^\./]+$/
 export const reactExtensionRE = /\.[tj]sx$/
-export const styleExtensionRE = /\.(styl|sass|scss|css)(?:\?scoped=(\w+))?$/
+export const styleExtensionRE =
+  /\.(styl|sass|scss|css)(?:\?(?:|.+?&)scoped=(\w+))?$/
 export const scopePseudoRE = /:scope\b/
 export const htmlPrefixRE = /^html\b/
